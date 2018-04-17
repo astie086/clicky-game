11f4b6b0a62b21632529630756ad699fda0f990c
astie086 <astie086@gmail.com>
2018-04-17 14:55:11 -0700

    Updates

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    console.log('hey queen');
+	$.backstretch([
+          "images/baron.jpg",
+          "images/DukeLeto.jpg",
+          "images/feyd.jpg",
+          "images/freeman.jpg",
+          "images/baron.jpg",
+          "images/duncan.jpg",
+          "images/Gaius.jpg",
+          "images/Rabban.jpg",
+          "images/Thufir.jpg",
+          "images/FaceDancer.jpg",
+          "images/emperor.jpg"
+          
+        ], {
+            duration: 10000,
+            fade: 900
+        });
+
+});
